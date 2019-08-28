1607db9fb40e16d0db3c0301ac0587fd039b5483
RAHUL THAKUR <iamrahulthakur.1992@gmail.com>
2019-08-28 14:37:54 +0530

    End of session 7

--- src/index.js
+++ src/index.js
@@ -2,21 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/layout/App';
+import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from './app/store/configureStore';
+
+const store = configureStore();
+
+console.log(store.getState());
 
 const rootEl = document.getElementById('root');
 
-let render = ()=>{
+let render = () => {
     ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    , rootEl);
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+        , rootEl);
 }
 
-if(module.hot){
-    module.hot.accept('./app/layout/App', ()=>{
+if (module.hot) {
+    module.hot.accept('./app/layout/App', () => {
         setTimeout(render);
     })
 }
