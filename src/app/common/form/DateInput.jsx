fee22b5bab9cc1934c1c02cddd0edb2d1bde56dd
RAHUL THAKUR <iamrahulthakur.1992@gmail.com>
2019-10-02 14:05:53 +0530

    date-fns prob

---
+++ src/app/common/form/DateInput.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Form, Label } from 'semantic-ui-react';
+import DatePicker from 'react-datepicker';
+import { format } from 'date-fns';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const DateInput = ({input, width, placeholder, dateFormat, meta: {touched, error}, ...rest}) => {
+    return (
+        <Form.Field error={touched && !!error}>
+            <DatePicker {...rest} placeholder={placeholder} selected={input.value ? format(new Date(input.value),dateFormat) : null} onChange={input.onChange} onBlur={input.onBlur} />
+            {touched && error && <Label basic color='red'>{error}</Label>}
+        </Form.Field>
+    )
+}
+
+export default DateInput;
