b2590d7107b3c1cd941f1c2f0d1d072a349a6ff5
RAHUL THAKUR <iamrahulthakur.1992@gmail.com>
2019-12-21 21:10:44 +0530

    End of section 11

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import {connect} from 'react-redux';
-import {incrementCounter, decrementCounter} from './testActions';
+import { connect } from 'react-redux';
+import { incrementCounter, decrementCounter } from './testActions';
 import { Button } from 'semantic-ui-react';
 import TestPlaceInput from './TestPlaceInput';
 import SimpleMap from './SimpleMap';
@@ -8,14 +8,16 @@ import {
     geocodeByAddress,
     getLatLng,
 } from 'react-places-autocomplete';
+import { openModal } from '../modals/modalActions';
 
-const mapState = (state)=>({
+const mapState = (state) => ({
     data: state.test.data
-}); 
+});
 
 const actions = {
     incrementCounter,
-    decrementCounter
+    decrementCounter,
+    openModal
 }
 
 class TestComponent extends Component {
@@ -28,24 +30,25 @@ class TestComponent extends Component {
 
     handleSelect = address => {
         geocodeByAddress(address)
-          .then(results => getLatLng(results[0]))
-          .then(latLng => {
-              this.setState({
-                  latlng: latLng
-              })
-          })
-          .catch(error => console.error('Error', error));
+            .then(results => getLatLng(results[0]))
+            .then(latLng => {
+                this.setState({
+                    latlng: latLng
+                })
+            })
+            .catch(error => console.error('Error', error));
     };
 
     render() {
-        const {data, incrementCounter, decrementCounter} = this.props;
+        const { data, incrementCounter, decrementCounter, openModal } = this.props;
         return (
             <div>
                 <h1>Test Component</h1>
                 <h3>The answer is: {data}</h3>
-                <Button onClick={incrementCounter} positive content='Increment'/>
-                <Button onClick={decrementCounter} negative content='Decrement'/>
-                <br/><br/>
+                <Button onClick={incrementCounter} positive content='Increment' />
+                <Button onClick={decrementCounter} negative content='Decrement' />
+                <Button onClick={() => openModal('TestModal', { data: 42 })} color='teal' content='Open Modal' />
+                <br /><br />
                 <TestPlaceInput selectAddress={this.handleSelect} />
                 <SimpleMap key={this.state.latlng.lng} latlng={this.state.latlng} />
             </div>
